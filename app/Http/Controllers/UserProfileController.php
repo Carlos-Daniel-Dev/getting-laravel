84245b9a5209db7acf6694b4dface59f4110fceb
user <carlosdaniel.programador@gmail.com>
2023-03-04 13:42:39 -0300

    more routes

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProdutoController extends Controller
+class UserProfileController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class ProdutoController extends Controller
      */
     public function index()
     {
-        return "index";
+        //
     }
 
     /**
@@ -45,7 +45,7 @@ class ProdutoController extends Controller
      */
     public function show($id)
     {
-        //
+        return $id;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProdutoController;
 
-Route::resource('produtos', ProdutoController::class);
+// Route::get('/', function() {
+//     return "home";
+// });
+
+// Route::get('/users/{id}', function($id) {
+//     return 'user ' . $id;
+// });
+
+// Route::get('/posts/{post}/comments/{comment}', function($post, $commentId) {
+//     return "post: " . $post . "commentID: " . $commentId;
+// });
+
+// Route::get('/user/{name?}', function($name = "John") {
+//     return $name;
+// });
+
+// Route::get('/users/{id}/{name}', function($id, $name) {
+//     return $id . $name;
+// })->whereNumber('id')->whereAlpha('name');
+
+// Route::get('/users/{id}', function($id) {
+//     return $id;
+// })->whereNumber('id');
+
+// Route::get('/search/{search}', function($search) {
+//     return $search;
+// })->where('search', '.*');
+
+Route::get('/user/profile',
+            [UserProfileController::class, 'show']
+)->name('profile');
+
+$url = route('profile', ['id' => 1]);
+    
