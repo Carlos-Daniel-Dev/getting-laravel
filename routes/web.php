60681ac66ed95d68d5a486795e2e5cc45f8d4888
user <carlosdaniel.programador@gmail.com>
2023-03-04 22:19:43 -0300

    middleware in routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
-use App\Http\Controllers\UserController;
+
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\UserStoreController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProductStoreController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ClientsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
-    
-Route::get('/user/create', [UserController::class, 'index']);
-Route::get('/user', UserStoreController::class);
-Route::resource('/product', ProductController::class)->except('store');
-Route::post('/product', ProductStoreController::class);
-Route::post('/product', ProductStoreController::class);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/protected', [HomeController::class, 'index'])->name('protected')->middleware('teste::alexandre');
+
+Route::prefix('admin')->middleware('auth')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.home')->withoutMiddleware('auth');
+    Route::get('/clients', [ClientsController::class, 'index'])->name('admin.clients');
+});
